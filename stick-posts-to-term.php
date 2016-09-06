0f166f27fe2d7e06c9989a2e8c744aa57eee6a5f
bambattajb <joe@white-fire.co.uk>
2016-09-06 13:06:52 +0100

    First release

--- stick-posts-to-term.php
+++ stick-posts-to-term.php
@@ -14,7 +14,7 @@
  * Plugin Name: Stick Posts To Tern
  * Plugin URI: 	http://joebuckle.me
  * Description: Simple term-specific sticky posts
- * Version:     0.0.3
+ * Version:     1.0.0
  * Author:      Joe Buckle
  * Author URI:  http://joebuckle.me
  * Text Domain: stick-posts-to-term
