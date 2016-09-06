9a7308d876557273bbed05919618299fdf24663a
bambattajb <joe@white-fire.co.uk>
2016-09-06 15:41:45 +0100

    First stable release Added composer support

--- src/StickyPost.php
+++ src/StickyPost.php
@@ -1,30 +1,4 @@
 <?php
-/**
- * Stick Posts To Term
- *
- * Simple tern-specific sticky posts
- *
- * @package   Stick_Posts_To_Term
- * @author    Joe Buckle <joe@white-fire.co.uk>
- * @license   GPL-2.0+
- * @link      http://joebuckle.me
- * @copyright 2016 Joe Buckle
- *
- * @wordpress-plugin
- * Plugin Name: Stick Posts To Tern
- * Plugin URI: 	http://joebuckle.me
- * Description: Simple term-specific sticky posts
- * Version:     1.0.0-stable
- * Author:      Joe Buckle
- * Author URI:  http://joebuckle.me
- * Text Domain: stick-posts-to-term
- * License:     GPL-2.0+
- * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
- */
-
-if ( ! defined( 'WPINC' ) ) {
-    die;
-}
 
 class StickyPost {
 
@@ -39,7 +13,12 @@ class StickyPost {
     );
 
     public static function allowed() {
-        return get_option('category-sticky-allowed');
+        $allowed = get_option('category-sticky-allowed');
+        if($allowed) {
+            return $allowed;
+        }
+
+        return array();
     }
 
     /**
@@ -368,7 +347,8 @@ class StickyPost {
 
     }
 
+    public static function theme_activate() {
+        add_action('init', 'StickyPost::get_instance');
+    }
+
 }
-
-
-add_action( 'plugins_loaded', array( 'StickyPost', 'get_instance' ) );
---
+++ wp-stick-posts-to-term.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Stick Posts To Term
+ *
+ * Simple tern-specific sticky posts
+ *
+ * @package   Stick_Posts_To_Term
+ * @author    Joe Buckle <joe@white-fire.co.uk>
+ * @license   GPL-2.0+
+ * @link      http://joebuckle.me
+ * @copyright 2016 Joe Buckle
+ *
+ * @wordpress-plugin
+ * Plugin Name: Stick Posts To Tern
+ * Plugin URI: 	http://joebuckle.me
+ * Description: Simple term-specific sticky posts
+ * Version:     1.0.1
+ * Author:      Joe Buckle
+ * Author URI:  http://joebuckle.me
+ * Text Domain: stick-posts-to-term
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+if(!class_exists('StickyPost')) {
+    include_once __DIR__ . '/src/StickyPost.php';
+}
+
+add_action( 'plugins_loaded', array( 'StickyPost', 'get_instance' ) );
